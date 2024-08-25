7f00c37e6e0c2e9f7788d50e2ba750351c68eaed
Shubham <thawareshubham1996@gmail.com>
2024-08-25 14:20:08 +0530

    updating tf modules

--- modules/ec2/key-pair.tf
+++
@@ -1,6 +0,0 @@
-# creating ssh-key.
-resource "aws_key_pair" "key-tf" {
-  key_name   = var.key_name
-  public_key = var.key
-
-}
--- modules/ec2/output.tf
+++
@@ -1,4 +0,0 @@
-output "ec2-id" {
-    value = aws_instance.server.id
-  
-}
