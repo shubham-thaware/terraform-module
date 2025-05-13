e0da7675ceb3a930364d87ae6ea993f64fef533a
Shubham Thaware <shubham-thware@email.com>
2025-05-13 15:47:04 +0530

    updated vpc module

--- vpc-tf-module/main.tf
+++ vpc-tf-module/main.tf
@@ -3,6 +3,7 @@ resource "aws_vpc" "vpc" {
   cidr_block           = var.vpc_cidr_block
   instance_tenancy     = "default"
   enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = merge(
     {
