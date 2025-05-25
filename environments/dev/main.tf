4e1e298d81aebf5b32b49bf412f586b93ba6b522
Shubham Thaware <160344293+shubham-thaware@users.noreply.github.com>
2025-05-25 23:38:02 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,6 +1,9 @@
 module "vpc" {
-  source         = "../aws-vpc"
+  source         = "../../aws-vpc"
   env            = var.env
   vpc_name       = var.vpc_name
   vpc_cidr_block = var.cidr_block
+  additional_tags = {
+    manage_by = "terraform"
+  }
 }
