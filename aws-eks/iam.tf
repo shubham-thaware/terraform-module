669c69c4c0bba40be024535fbcd6efaf80a6f039
Shubham Thaware <160344293+shubham-thaware@users.noreply.github.com>
2025-06-04 12:42:36 +0530

    Update iam.tf

--- aws-eks/iam.tf
+++ aws-eks/iam.tf
@@ -1,6 +1,5 @@
-#AWS EKS Cluster IAM Role
 resource "aws_iam_role" "cluster" {
-  name = var.aws_eks_cluster_name
+  name = "${var.aws_eks_cluster_name}-eks-cluster-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -21,16 +20,19 @@ resource "aws_iam_role" "cluster" {
 # Attach EKS Cluster Policies
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks_cluster.name
+  role       = aws_iam_role.cluster.name
+  depends_on = [aws_iam_role.cluster]
 }
 
 resource "aws_iam_role_policy_attachment" "eks_cluster_vpc_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-  role       = aws_iam_role.eks_cluster.name
+  role       = aws_iam_role.cluster.name
+  depends_on = [aws_iam_role.cluster]
 }
 resource "aws_iam_role_policy_attachment" "eks_service_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = aws_iam_role.eks_cluster.name
+  role       = aws_iam_role.cluster.name
+  depends_on = [aws_iam_role.cluster]
 }
 
 # EKS Node IAM Role
@@ -53,14 +55,17 @@ resource "aws_iam_role" "eks_nodes" {
 resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role       = aws_iam_role.eks_nodes.name
+  depends_on = [aws_iam_role.eks_nodes] 
 }
 
 resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   role       = aws_iam_role.eks_nodes.name
+  depends_on = [aws_iam_role.eks_nodes] 
 }
 
 resource "aws_iam_role_policy_attachment" "ec2_container_registry_readonly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.eks_nodes.name
+  depends_on = [aws_iam_role.eks_nodes] 
 }
