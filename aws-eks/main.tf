86709a811e70df7e22b0b000bab7524dbc4a307f
Shubham Thaware <160344293+shubham-thaware@users.noreply.github.com>
2025-06-04 12:28:07 +0530

    Update main.tf

--- aws-eks/main.tf
+++ aws-eks/main.tf
@@ -1,16 +1,14 @@
-# EKS Cluster
 resource "aws_eks_cluster" "cluster" {
   name     = var.aws_eks_cluster_name
   role_arn = aws_iam_role.eks_cluster.arn
   version  = var.aws_eks_cluster_version
   vpc_config {
     subnet_ids              = var.aws_vpc_private_subnet_ids
-    endpoint_private_access = true
-    endpoint_public_access  = false
+    endpoint_private_access = var.eks_endpoint_private_access
+    endpoint_public_access  = var.eks_endpoint_public_access
     public_access_cidrs     = var.cluster_endpoint_public_access_cidrs
-    security_group_ids      = [var.aws_eks_cluster_sg_ids]
+    security_group_ids      = var.aws_eks_cluster_sg_ids  
   }
-
   depends_on = [
     aws_iam_role_policy_attachment.eks_cluster_policy,
     aws_iam_role_policy_attachment.eks_cluster_vpc_policy,
@@ -18,27 +16,18 @@ resource "aws_eks_cluster" "cluster" {
   ]
 }
 
-
-
-
-
-
-
-# EKS Node Group (2 t2.medium nodes)
-# EKS Node Group (1 t3.medium node)
 resource "aws_eks_node_group" "nodes" {
   cluster_name    = aws_eks_cluster.cluster.name
   node_group_name = "${var.aws_eks_cluster_name}-eks-node-group"
   node_role_arn   = aws_iam_role.eks_nodes.arn
   subnet_ids      = var.aws_vpc_private_subnet_ids
- #version         = var.aws_eks_cluster_version (optional)
-  instance_types  = ["t3.medium"]        # Changed from t2.medium
-  ami_type        = "AL2_x86_64"         # Optional: Use Amazon Linux 2 for better compatibility
+  version         = var.aws_eks_cluster_version 
+  instance_types  = [var.eks_instance_type]
 
   scaling_config {
-    desired_size = 1
-    max_size     = 4
-    min_size     = 1
+    desired_size = var.eks_desired_size
+    max_size     = var.eks_max_size
+    min_size     = var.eks_min_size
   }
 
   depends_on = [
