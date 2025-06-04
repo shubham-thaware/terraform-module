3086aff9f22f0f03fcac3db1d69b65e1e09cc139
Shubham Thaware <160344293+shubham-thaware@users.noreply.github.com>
2025-06-04 11:30:18 +0530

    Delete environments directory

--- environments/dev/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "ci-cd-terraform-state"      # replace with your S3 bucket
-    key            = "dev/vpc/terraform.tfstate"       # unique key per env/module
-    region         = "us-east-1"                       # your AWS region
-    dynamodb_table = "terraform-locks"                 # must exist in S3
-    encrypt        = true
-  }
-}
