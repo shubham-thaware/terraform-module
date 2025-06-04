369c2b3971fa054e77170801fee1702129168aa3
Shubham Thaware <160344293+shubham-thaware@users.noreply.github.com>
2025-06-04 12:02:04 +0530

    Update variable.tf

--- aws-security-groups/variable.tf
+++ aws-security-groups/variable.tf
@@ -3,7 +3,8 @@ variable "vpc_id" {
   type        = string
 }
 
-variable "cluster_name" {
-  description = "EKS cluster name"
-  type        = string
+variable "aws_eks_cluster_name" {
+    description = "Name of the EKS cluster"
+    type = string
+  
 }
