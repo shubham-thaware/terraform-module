d7ba97c5d0eb33ad543f8866eaaf2bc0c9048b83
Shubham Thaware <160344293+shubham-thaware@users.noreply.github.com>
2025-06-08 18:42:13 +0530

    Update output.tf

--- aws-vpc/output.tf
+++ aws-vpc/output.tf
@@ -9,9 +9,9 @@ output "vpc_arn" {
 }
 
 output "private_subnet_ids" {
-  value = [aws_subnet.private-subnet-1.id, aws_subnet.private-subnet-2.id]
+  value = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
 }
 
 output "public_subnet_ids" {
-  value = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id]
+  value = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
 }
