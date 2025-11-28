b76269f963d181fda5316b95c810ac79f4382335
SHUBHAM <SHUBHAM@GMAIL.COM>
2025-11-28 19:04:03 +0530

    update

--- aws-eks/output.tf
+++ aws-eks/output.tf
@@ -35,22 +35,6 @@ output "cluster_primary_security_group_id" {
   value = aws_eks_cluster.this.vpc_config[0].cluster_security_group_id
 }
 
-# Node Group outputs (default)
-output "node_group_public_arn" {
-  value = aws_eks_node_group.default.arn
-}
-
-output "node_group_public_id" {
-  value = aws_eks_node_group.default.id
-}
-
-output "node_group_public_status" {
-  value = aws_eks_node_group.default.status
-}
-
-output "node_group_public_version" {
-  value = aws_eks_node_group.default.version
-}
 
 output "node_group_1_arn" {
   value = aws_eks_node_group.ng_1.arn
