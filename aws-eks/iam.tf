a78f17d6c6c0db0be1e96a2d0eddcc6b1fdb0a4d
Shubham Thaware <160344293+shubham-thaware@users.noreply.github.com>
2025-11-28 20:49:58 +0530

    Update iam.tf

--- aws-eks/iam.tf
+++ aws-eks/iam.tf
@@ -5,7 +5,7 @@ resource "aws_iam_role" "cluster" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
-      Action    = ["sts:AssumeRole", "sts:TagSession"],
+      Action    = ["sts:AssumeRole"],
       Effect    = "Allow",
       Principal = { Service = "eks.amazonaws.com" }
     }]
