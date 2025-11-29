adfbbc07eb873a5c94be18f36e78760534d18b45
Shubham Thaware <160344293+shubham-thaware@users.noreply.github.com>
2025-11-29 10:42:44 +0530

    Update main.tf

--- aws-security-groups/main.tf
+++ aws-security-groups/main.tf
@@ -59,7 +59,12 @@ resource "aws_security_group" "eks_cluster" {
   name        = "${var.aws_eks_cluster_name}-cluster-sg"
   description = "Cluster communication with worker nodes"
   vpc_id      = var.vpc_id
-
+  ingress {
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
