c6e750a78a39ab100ed1b94ee6a6b2264eb8d21e
Shubham Thaware <160344293+shubham-thaware@users.noreply.github.com>
2026-05-27 20:55:00 +0530

    Update addons.tf

--- aws-eks/addons.tf
+++ aws-eks/addons.tf
@@ -1,3 +1,6 @@
+# ==========================================
+# CoreDNS Addon
+# ==========================================
 data "aws_eks_addon_version" "coredns" {
   addon_name         = "coredns"
   kubernetes_version = aws_eks_cluster.this.version
@@ -5,35 +8,42 @@ data "aws_eks_addon_version" "coredns" {
 }
 
 resource "aws_eks_addon" "coredns" {
-  cluster_name  = aws_eks_cluster.this.name
-  addon_name    = "coredns"
-  addon_version = data.aws_eks_addon_version.coredns.version
+  cluster_name                = aws_eks_cluster.this.name
+  addon_name                  = "coredns"
+  addon_version               = data.aws_eks_addon_version.coredns.version
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
-    depends_on = [
-    aws_eks_cluster.this
-  ]
+
+  # SAFEGUARD: If you delete this terraform stack, don't break cluster DNS
+  preserve = true 
+
+  depends_on = [aws_eks_cluster.this]
 }
 
-# kube-proxy
+# ==========================================
+# Kube-Proxy Addon
+# ==========================================
 data "aws_eks_addon_version" "kube_proxy" {
   addon_name         = "kube-proxy"
   kubernetes_version = aws_eks_cluster.this.version
   most_recent        = true
 }
+
 resource "aws_eks_addon" "kube_proxy" {
-  cluster_name  = aws_eks_cluster.this.name
-  addon_name    = "kube-proxy"
-  addon_version = data.aws_eks_addon_version.kube_proxy.version
+  cluster_name                = aws_eks_cluster.this.name
+  addon_name                  = "kube-proxy"
+  addon_version               = data.aws_eks_addon_version.kube_proxy.version
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
-    depends_on = [
-    aws_eks_cluster.this
-  ]
+  
+  preserve = true
+
+  depends_on = [aws_eks_cluster.this]
 }
 
-
-# pick latest version AWS supports for current cluster version
+# ==========================================
+# VPC-CNI Addon
+# ==========================================
 data "aws_eks_addon_version" "vpc_cni" {
   addon_name         = "vpc-cni"
   kubernetes_version = aws_eks_cluster.this.version
@@ -41,21 +51,20 @@ data "aws_eks_addon_version" "vpc_cni" {
 }
 
 resource "aws_eks_addon" "vpc_cni" {
-  cluster_name  = aws_eks_cluster.this.name
-  addon_name    = "vpc-cni"
-  addon_version = data.aws_eks_addon_version.vpc_cni.version
-
-  # recommended: let AWS manage conflicts by overwriting if necessary
+  cluster_name                = aws_eks_cluster.this.name
+  addon_name                  = "vpc-cni"
+  addon_version               = data.aws_eks_addon_version.vpc_cni.version
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
+  
+  preserve = true
 
-  # optional: you can add tags or configuration_values here
-  depends_on = [
-    aws_eks_cluster.this
-  ]
+  depends_on = [aws_eks_cluster.this]
 }
 
-#EBS CSI Driver
+# ==========================================
+# EBS CSI Driver Addon
+# ==========================================
 data "aws_eks_addon_version" "ebs_csi" {
   addon_name         = "aws-ebs-csi-driver"
   kubernetes_version = aws_eks_cluster.this.version
@@ -63,12 +72,10 @@ data "aws_eks_addon_version" "ebs_csi" {
 }
 
 resource "aws_eks_addon" "ebs_csi" {
-  cluster_name  = aws_eks_cluster.this.name
-  addon_name    = "aws-ebs-csi-driver"
-  addon_version = data.aws_eks_addon_version.ebs_csi.version
-
-  service_account_role_arn = aws_iam_role.ebs_csi_irsa.arn
-
+  cluster_name                = aws_eks_cluster.this.name
+  addon_name                  = "aws-ebs-csi-driver"
+  addon_version               = data.aws_eks_addon_version.ebs_csi.version
+  service_account_role_arn    = aws_iam_role.ebs_csi_irsa.arn
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
 
@@ -77,4 +84,3 @@ resource "aws_eks_addon" "ebs_csi" {
     aws_iam_role_policy_attachment.ebs_csi_policy
   ]
 }
-
